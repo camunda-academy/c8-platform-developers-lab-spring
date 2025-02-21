9f313666121327210640d1c73ee877760123709f
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2025-02-21 14:48:46 +0100

    several updates

---
+++ src/main/test/java/com/camunda/academy/C8PlatformDevelopersLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camunda.academy.c8_platform_developers_lab_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C8PlatformDevelopersLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
